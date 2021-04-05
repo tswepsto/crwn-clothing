a764ebcfaa0b77ef3f505e16cc15a6f551869d4c
Todd Swepston <tswepston@armadapower.com>
2021-04-05 08:07:35 -0400

    This is just to see how commits work in VS CODE

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import HomePage from "./pages/homepage/Homepage";
 import { Route, Switch } from "react-router-dom";
 
+// This is a test just to see how git commits work in VS CODE
+
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
